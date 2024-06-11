573ee69cf216d9c4adbabe3fe9cbc2d20ee6117c
kashakesho <keshko123456@gmail.com>
2024-06-11 20:50:22 +0300

    delete brgola

--- controllers/hospitalManager.js
+++ controllers/hospitalManager.js
@@ -42,7 +42,7 @@ exports.appointSurgery = async (req, res, next) => {
       doctorID,
       patientID,
     });
-    const deletee = surgeries.findByIdAndDelete({ _id: surgeryID });
+    const deletee = await surgeries.findByIdAndDelete({ _id: surgeryID });
     res.json(creation);
   } else {
     const error = new Error("Cannot appoint on this day");
